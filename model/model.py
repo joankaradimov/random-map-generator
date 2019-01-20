84c1ce3c414618646d819fdf06f59dade104ee2a
Joan Karadimov <joan.karadimov@gmail.com>
2019-01-20 16:14:38 +0200

    Demonstrate by plotting instead of logging

--- model/model.py
+++ model/model.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import mpq
 import numpy as np
 import os
@@ -6,8 +7,6 @@ import config
 from scenario import *
 from tileset import Tileset
 
-print('Jungle tiles:', len(Tileset.JUNGLE.tiles))
-
 scenarios = []
 scenarios += process_scenarios(os.path.join(config.STARCRAFT_ROOT, 'Maps'))
 for directory in config.MAP_DIRECTORIES:
@@ -15,6 +14,18 @@ for directory in config.MAP_DIRECTORIES:
 
 four_player_jungle_scenarios = [x for x in scenarios if x.human_players == 4 and x.tileset == Tileset.JUNGLE and x.width == 128 and x.height == 128]
 
-print('DONE.')
-print('Total scenarios:', len(scenarios))
-print('Four player jungle scenarios:', len(four_player_jungle_scenarios))
+def tile_average_height(tile):
+
+    @np.vectorize
+    def minitile_heights(minitile):
+        return minitile.height
+
+    return np.average(minitile_heights(tile.minitiles))
+
+scenario = four_player_jungle_scenarios[13]
+tile_heights = np.vectorize(tile_average_height)(scenario.tiles)
+
+plt.figure()
+plt.suptitle(scenario.filename)
+plt.imshow(tile_heights, cmap=plt.cm.Blues)
+plt.show()
