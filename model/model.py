d7b17c917209827a84f348250ac78a54103818c7
Joan Karadimov <joan.karadimov@gmail.com>
2019-01-21 22:04:39 +0200

    Use walkability when displaying a scenario height map

--- model/model.py
+++ model/model.py
@@ -14,18 +14,22 @@ for directory in config.MAP_DIRECTORIES:
 
 four_player_jungle_scenarios = [x for x in scenarios if x.human_players == 4 and x.tileset == Tileset.JUNGLE and x.width == 128 and x.height == 128]
 
-def tile_average_height(tile):
+def get_height_map(tile):
 
     @np.vectorize
     def minitile_heights(minitile):
         return minitile.height
 
-    return np.average(minitile_heights(tile.minitiles))
+    @np.vectorize
+    def minitile_walkability(minitile):
+        return minitile.walkable / 2
+
+    return np.average(minitile_heights(tile.minitiles) + minitile_walkability(tile.minitiles)) + 0.0001
 
 scenario = four_player_jungle_scenarios[13]
-tile_heights = np.vectorize(tile_average_height)(scenario.tiles)
+height_map = np.vectorize(get_height_map)(scenario.tiles)
 
 plt.figure()
 plt.suptitle(scenario.filename)
-plt.imshow(tile_heights, cmap=plt.cm.Blues)
+plt.imshow(height_map, cmap=plt.cm.Blues)
 plt.show()
