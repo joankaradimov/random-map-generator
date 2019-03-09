401bfdc60a013bdd5bac8efa992bfac7fa14ad67
Joan Karadimov <joan.karadimov@gmail.com>
2019-03-10 01:29:25 +0200

    Process raw chk files

--- model/game.py
+++ model/game.py
@@ -16,6 +16,12 @@ class Game:
 
         for dirName, subdirList, fileList in os.walk(directory):
             for filename in fileList:
+                if filename.endswith('.chk'):
+                    chk_path = os.path.join(dirName, filename)
+                    with open(chk_path, 'rb') as chk_file:
+                        scenario = self.process_chk(filename, chk_file)
+                        if scenario != None:
+                            scenarios.append(scenario)
                 if filename.endswith('.scm') or filename.endswith('.scx'):
                     scenario = self.process_scenario(os.path.join(dirName, filename))
                     if scenario != None:
@@ -28,8 +34,11 @@ class Game:
             map = mpq.MPQFile(scenario_path)
             chk_file = map.open('staredit\\scenario.chk')
             filename = os.path.basename(scenario_path)
-            return ScenarioBuilder(filename, chk_file).to_scenario()
+            return self.process_chk(filename, chk_file)
         except Exception as e:
             pass # TODO: parse protected scenarios
         finally:
             chk_file.close()
+
+    def process_chk(self, filename, chk_file):
+        return ScenarioBuilder(filename, chk_file).to_scenario()
