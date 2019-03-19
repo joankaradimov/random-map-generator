b6b9cbe6505e2a96bcb1be3d2ace4f50a653e487
Joan Karadimov <joan.karadimov@gmail.com>
2019-03-20 01:03:46 +0200

    Implement campaign scenarios parsing

--- model/game.py
+++ model/game.py
@@ -2,14 +2,28 @@ import mpq
 import os
 
 from scenario import *
+from string_table import *
 
 class Game:
     def __init__(self, game_directory):
         self.directory = game_directory
 
     def process_game_scenarios(self):
-        maps_directory = os.path.join(self.directory, 'Maps')
-        return self.process_scenarios(maps_directory)
+        scenarios = []
+        data = mpq.MPQFile()
+        try:
+            data.add_archive(os.path.join(self.directory, 'Starcraft.mpq'))
+            data.add_archive(os.path.join(self.directory, 'Broodwar.mpq'))
+            data.add_archive(os.path.join(self.directory, 'StarDat.mpq'))
+            data.add_archive(os.path.join(self.directory, 'BrooDat.mpq'))
+            data.add_archive(os.path.join(self.directory, 'patch_rt.mpq'))
+            data.add_archive(os.path.join(self.directory, 'patch_ed.mpq'))
+            scenarios += self.process_mpq(data)
+        finally:
+            data.close()
+
+        scenarios += self.process_scenarios(os.path.join(self.directory, 'Maps'))
+        return scenarios
 
     def process_scenarios(self, directory):
         scenarios = []
@@ -42,3 +56,27 @@ class Game:
 
     def process_chk(self, filename, chk_file):
         return Scenario.builder(filename, chk_file).to_scenario()
+
+    def process_mpq(self, data):
+        scenarios = []
+        map_data = data.open('arr\mapdata.tbl')
+        try:
+            chk_files = StringTable(map_data)
+        except mpq.storm.error as e:
+            return scenarios
+        finally:
+            map_data.close()
+
+        for filename in chk_files:
+            chk_file = None
+            try:
+                chk_file = data.open(filename + '\\staredit\\scenario.chk')
+                scenario = self.process_chk(os.path.basename(filename), chk_file)
+                scenarios.append(scenario)
+            except Exception as e:
+                pass
+            finally:
+                if chk_file != None:
+                    chk_file.close()
+
+        return scenarios
