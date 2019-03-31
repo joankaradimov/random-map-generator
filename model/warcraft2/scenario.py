23b5bd425e42875a59767a26d542e104b2fff1be
Joan Karadimov <joan.karadimov@gmail.com>
2019-04-01 00:27:06 +0300

    Correctly handle Warcraft 2 scenario descriptions

--- model/warcraft2/scenario.py
+++ model/warcraft2/scenario.py
@@ -1,11 +1,13 @@
 import scenario
 
 class ScenarioBuilder(scenario.ScenarioBuilder):
+    def handle_DESC(self, data):
+        self.description = data.decode('ISO-8859-1').strip('\0')
+
     def to_scenario(self):
         self.process_MTMX()
         # TODO: correctly parse these
         self.name = ''
-        self.description = ''
         self.alliances = 1
         self.strings = []
 
