30d7424b8ac2127dfd5377f132b43db77a8f1771
Joan Karadimov <joan.karadimov@gmail.com>
2019-09-03 09:51:35 +0300

    Remove unneeded inheritance from the Warcraft 1 game

--- model/warcraft/game.py
+++ model/warcraft/game.py
@@ -1,7 +1,7 @@
 # TODO: implement this using the specs from here:
 # http://atariage.com/forums/topic/154725-warcraft-map-data/
 
-class Game(game.MpqBasedGame):
+class Game:
     @classmethod
     def data_files(cls):
         return ['data/data.war']
