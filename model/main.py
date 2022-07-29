2cdf541d0d1f105ea43a76e29e5e7187c78e4801
Joan Karadimov <joan.karadimov@gmail.com>
2022-07-30 01:11:54 +0300

    Fix Game instantiation

--- model/main.py
+++ model/main.py
@@ -5,7 +5,7 @@ from game import Game
 
 if __name__ == '__main__':
     scenarios = []
-    game = Game.create(config.STARCRAFT_ROOT)
+    game = Game(config.STARCRAFT_ROOT)
     scenarios += game.process_game_scenarios()
     for directory in config.MAP_DIRECTORIES:
         scenarios += game.process_directory(directory)
